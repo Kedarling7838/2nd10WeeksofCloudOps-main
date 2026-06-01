af356483b1aabb26caec66e6d75a381f4f75f62c
Kedarling7838 <kedarlingkanade@gmail.com>
2026-06-01 09:23:07 +0530

    Delete ROOT-terraform directory

--- ROOT-terraform/modules/database/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "db_instance_identifier" {
-  description = "RDS instance identifier."
-  value       = aws_db_instance.books-rds.identifier
-}
-
-output "db_endpoint" {
-  description = "RDS endpoint."
-  value       = aws_db_instance.books-rds.endpoint
-}
